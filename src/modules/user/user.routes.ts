de4b8174cc6827259ecc34a9ca9ab9a7aaadb2f7
devsafix <devsafix@gmail.com>
2025-09-15 10:28:33 +0600

    user route added

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post("/", UserController.createUser);
 router.get("/", UserController.getAllUser);
 router.get("/:id", UserController.getUserById);
+router.patch("/:id", UserController.updateUser);
+router.delete("/:id", UserController.deleteUser);
 
 export const UserRouter = router;
