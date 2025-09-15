4a546e4143f14d1dfde36de6a9d315e5acf7c58c
devsafix <devsafix@gmail.com>
2025-09-15 11:54:38 +0600

    pagination and filtering added in get all post api

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -18,8 +18,50 @@ const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
   return result;
 };
 
-const getAllPosts = async () => {
-  const result = await prisma.post.findMany();
+const getAllPosts = async ({
+  page = 1,
+  limit = 10,
+  search = "",
+  isFeatured,
+  tags,
+}: {
+  page?: number;
+  limit?: number;
+  search?: string;
+  isFeatured?: boolean;
+  tags?: string[];
+}) => {
+  const skip = (page - 1) * limit;
+  const normalizedSearch = search.trim();
+
+  const where: any = {
+    AND: [
+      normalizedSearch && {
+        OR: [
+          {
+            title: { contains: normalizedSearch, mode: "insensitive" },
+          },
+          {
+            content: { contains: normalizedSearch, mode: "insensitive" },
+          },
+        ],
+      },
+      typeof isFeatured === "boolean" && { isFeatured },
+      tags &&
+        tags?.length > 0 && {
+          tags: {
+            hasEvery: tags,
+          },
+        },
+    ].filter(Boolean),
+  };
+
+  const result = await prisma.post.findMany({
+    skip,
+    take: limit,
+    where,
+  });
+
   return result;
 };
 
