143f45cb192c97d56d031f7f0f5fe453f873db83
devsafix <devsafix@gmail.com>
2025-11-16 06:48:45 +0600

    Feat: Add Zod Validation For User and Auth

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { AuthController } from "./auth.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { loginSchema } from "./auth.validation";
 
 const router = express.Router();
 
-router.post("/login", AuthController.loginWithEmailAndPassword);
+router.post(
+  "/login",
+  validateRequest(loginSchema),
+  AuthController.loginWithEmailAndPassword
+);
 router.post("/google", AuthController.authWithGoogle);
 
 export const AuthRouter = router;
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { UserController } from "./user.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createUserSchema } from "./user.validation";
 
 const router = express.Router();
 
-router.post("/", UserController.createUser);
+router.post("/", validateRequest(createUserSchema), UserController.createUser);
 router.get("/", UserController.getAllUser);
 router.get("/:id", UserController.getUserById);
 router.patch("/:id", UserController.updateUser);
